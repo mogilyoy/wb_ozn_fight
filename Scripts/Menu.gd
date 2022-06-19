c1c784dd7e94f8237f4e491ca457c8c3a00725c8
KWARC <koc-pas@yandex.ru>
2022-06-20 03:20:47 +0700

    Создал не рабочую игру бля
    Создал бля код для старта игры бля, а не работает бля

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
 func _ready():
-	pass # Replace with function body.
+	get_tree().connect('network_peer_connected', self, '_connected')
 
 
 
 func _on_Server_creating_pressed():
-	pass # Replace with function body.
+	var server = NetworkedMultiplayerENet.new()
+	server.create_server(7777, 5)
+	get_tree().set_network_peer(server)
+	print('created')
 
 
 func _on_Server_loading_pressed():
-	pass # Replace with function body.
+	var client = NetworkedMultiplayerENet.new()
+	client.create_client('127.0.0.1', 7777)
+	get_tree().set_network_peer(client)
+	print('loaded')
+
+func _connected(client_id):
+	Singleteon.user_id = client_id
+	var game = preload('res://Scens/Сцена нищих тестов.tscn').instance()
+	get_tree().get_root().add_child(game)
+	print('connected')
+	hide()
