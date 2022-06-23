53b6be7619296109743262ef164dfba0a31107a1
mogilyoy <mogilyoy@ya.ru>
2022-06-23 19:43:57 +0700

    девачки бегают туда сюда

--- Scripts/BadGuy.gd
+++ Scripts/BadGuy.gd
@@ -7,8 +7,12 @@ var vel: Vector3 = Vector3()  # вектор скорости челикса
 var gravity: float = 20.0
 var health = 200
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	vel.z = rand_range(-10,10)
+	vel.x = rand_range(-10,10)
+	
 	pass # Replace with function body.
 
 
@@ -20,7 +24,13 @@ func _process(delta):
 func _physics_process(delta):
 	girl_anim.play('ArmatureAction')
 	vel.y -= gravity * delta
-	vel.z = rand_range(-10,10)
 	vel = move_and_slide(vel, Vector3.UP)
-	self.rotate_y(deg2rad(rand_range(0.01, 0.1)))
+	if vel.z < 2 and vel.z > -2:
+		vel.z = rand_range(-10, 10)
+		self.rotate_y(vel.z)
+		
+	if vel.x <2 and vel.x > -2:
+		vel.x = rand_range(-10, 10)
+		self.rotate_y(vel.x)
+	
 	
