d2e8401e54030b8ea7a11f9869a8431ba51cfe3a
mogilyoy <mogilyoy@ya.ru>
2022-06-24 21:47:55 +0700

    мелкий визуал
    анимации движения, оружия

--- Game Client/Scripts/ПУШКА.gd
+++ Game Client/Scripts/ПУШКА.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 const ADS_LERP = 20
+const damage = 500
+const anim_speed = 1
 
 onready var animation = $Anim
 onready var sound = $BAX
@@ -8,13 +10,10 @@ onready var muzzle = $Muzzle
 onready var raycast = $RayCast
 onready var bullet = preload("res://Scens/Пуля дура.tscn") # загружаем в игру пулю
 
-
 var mag = 30
 
 export var default_positon: Vector3
 export var ads_positon: Vector3
-
-var fv = {'Default': 38.5, 'ADS': 20}
 
 onready var timer = get_node("Timer")
 
@@ -24,25 +23,28 @@ func _ready():
 	pass
 
 func _physics_process(delta): 
-				
 	if Input.is_action_pressed("aim"):
 		transform.origin = transform.origin.linear_interpolate(ads_positon, ADS_LERP * delta)
 	else:
 		transform.origin = transform.origin.linear_interpolate(default_positon, ADS_LERP * delta)
 		
+	if Input.is_action_pressed("move_forward"):
+		if animation.is_playing():
+			pass
+		else:
+			animation.play("Шаг")
+	if Input.is_action_just_released("move_forward"):
+		if animation.is_playing():
+			pass
+		else:
+			animation.stop()
+		
 	if Input.is_action_just_pressed("reload"):
 		mag = 30
+			
+func GunAnimation():
+	animation.stop()
+	animation.play("Отдача")
 
-func ShootGun():
-	# if mag > 0:
-		if raycast.is_colliding():
-			var b = bullet.instance()
-			muzzle.add_child(b)
-			b.look_at(raycast.get_collision_point(), Vector3.UP)
-			b.SHOOT()
-			mag -= 1
-			animation.stop()
-			animation.play("Отдача") 
 
-func _on_Timer_timeout():
-	pass 
+		
